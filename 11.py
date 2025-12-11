8b5430663bbfde3ea5c735aba4b471d50f5b8162
Nick Yeow <ytknick@gmail.com>
2025-12-11 18:13:12 +0800

    Day 11 in Python

---
+++ 11.py
@@ -0,0 +1,27 @@
+graph = {}
+
+with open('11.txt') as f:
+    for line in f:
+        node, neighbours = line.split(': ')
+        graph[node] = neighbours.split()
+
+memo = {}
+
+def paths(start, end):
+    if (start, end) in memo:
+        return memo[start, end]
+    if start == end:
+        return 1
+    result = sum(
+        paths(neighbour, end)
+        for neighbour in graph.get(start, [])
+    )
+    if result not in memo:
+        memo[start, end] = result
+    return result
+
+print(paths('you', 'out'))
+print(
+    paths('svr', 'dac') * paths('dac', 'fft') * paths('fft', 'out') +
+    paths('svr', 'fft') * paths('fft', 'dac') * paths('dac', 'out')
+)
